6fdf11de31fb83dcf07dd02b489ced0a8ef6a161
Kartik Nillawar <kartiknillawar@gmail.com>
2021-08-20 21:57:06 +0530

    modified the project

--- textutils/views.py
+++ textutils/views.py
@@ -6,12 +6,12 @@ def index(request):
 #   render(request,filename)
 
 def analyze(request):
-    text1 = request.GET.get('text', 'default')
-    removepunc = request.GET.get('removepun','off')
-    fullcap = request.GET.get('fullcaps','off')
-    spaceremover = request.GET.get('spaceremover','off')
-    newlineremove = request.GET.get('newlineremove','off')
-    countchar = request.GET.get('countchar','off')
+    text1 = request.POST.get('text', 'default')
+    removepunc = request.POST.get('removepun','off')
+    fullcap = request.POST.get('fullcaps','off')
+    spaceremover = request.POST.get('spaceremover','off')
+    newlineremove = request.POST.get('newlineremove','off')
+    countchar = request.POST.get('countchar','off')
 
     print(removepunc)
     if removepunc == "on":
@@ -20,34 +20,42 @@ def analyze(request):
         for char in text1:
             if char not in punctuations:
                 analyzed = analyzed + char
+
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif fullcap=="on":
+        text1 = analyzed;
+        #return render(request, 'analyze.html', params)
+    if fullcap=="on":
         analyzed =""
         for char in text1:
             analyzed = analyzed + char.upper()
         params = {'purpose': 'charecter are capatalized', 'analyzed_text': analyzed}
-        return  render(request,'analyze.html',params)
-    elif spaceremover=="on":
+        text1 = analyzed;
+        #return  render(request,'analyze.html',params)
+    if spaceremover=="on":
         analyzed = ""
         for index,char in enumerate(text1):
             if not(text1[index]==" "and text1[index+1]==" "):
                 analyzed = analyzed + char
         params = {'purpose': 'Extra space removed', 'analyzed_text': analyzed}
-        return  render(request,'analyze.html',params)
-    elif newlineremove=="on":
+        text1 = analyzed;
+        #return  render(request,'analyze.html',params)
+    if newlineremove=="on":
         analyzed = ""
         for char in text1:
-            if char:
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
+
         params = {'purpose': 'Extra space removed', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-    elif countchar=="on":
+        text1 = analyzed;
+        #return render(request, 'analyze.html', params)
+    if countchar=="on":
         count = 0
         for char in text1:
             if char.isalpha():
                 count = count + 1
         params = {'purpose': 'Total charecter', 'analyzed_text': count}
-        return render(request, 'analyze.html', params)
-    else:
-        return HttpResponse("Error")
+        text1 = analyzed;
+        #return render(request, 'analyze.html', params)
+    if (removepunc != "on" and countchar != "on" and newlineremove != "on" and spaceremover != "on"):
+        return HttpResponse("please select any operation and try again")
+    return render(request, 'analyze.html', params)
